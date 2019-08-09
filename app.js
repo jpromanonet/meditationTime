9feddb3c603b8121e98f11e7a53fbae5a6f6aa56
jpromanonet <programming@jpromano.net>
2019-08-08 22:13:50 -0300

    Player added

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ const app = () => {
   const timeDisplay = document.querySelector(".time-display");
 
   // Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+
+  // Duration
+  let fakeDuration = 600;
+
+  // Timer animation
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  // Play sounds
+  play.addEventListener("click", () => {
+    checkPlay(song);
+  });
+
+  // Function to stop and play sounds and change icons.
+  const checkPlay = song => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
 };
 
 app();
